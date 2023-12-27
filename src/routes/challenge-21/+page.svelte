b219b2f1190e301916af8e6ecf27f0dd52ce1242
oleksii-dukhovenko <oleksii.dukhovenko@r2pos.com>
2023-12-27 14:38:47 -0500

    Weather-U.I.Changes

--- src/routes/challenge-21/+page.svelte
+++ src/routes/challenge-21/+page.svelte
@@ -1,105 +1,234 @@
 <script>
-    import { onMount } from 'svelte';
-    import { fly, fade } from 'svelte/transition';
-    import HourlyWeatherButton from '$lib/components/HourlyWeatherButton.svelte';
-    import DailyWeatherButton from '$lib/components/DailyWeatherButton.svelte';
-    import { getWeatherData } from './server';
-
+    import { onMount } from "svelte";
+    import { fade } from "svelte/transition";
+    import { getWeatherData } from "./server";
 
     let hourlyWeather;
     let dailyWeather;
-    let showHourly = false;
-    let showDaily = false;
+    let hourlyWeatherByDay = {};
+    let activeTab = "hourly";
 
     onMount(async () => {
         const myWeatherData = await getWeatherData();
         hourlyWeather = myWeatherData.hourly;
         dailyWeather = myWeatherData.daily;
+        groupHourlyByDay();
     });
 
-    function toggleHourly() {
-        showHourly = !showHourly;
-        if (showHourly) showDaily = false; 
+    function groupHourlyByDay() {
+        if (
+            !hourlyWeather ||
+            !hourlyWeather.time ||
+            !hourlyWeather.temperature2m ||
+            !hourlyWeather.rain
+        ) {
+            console.error("Invalid hourlyWeather data structure");
+            return;
+        }
+
+        hourlyWeatherByDay = hourlyWeather.time.reduce((acc, time, index) => {
+            const day = new Date(time).toDateString();
+            if (!acc[day]) {
+                acc[day] = [];
+            }
+            acc[day].push({
+                time: new Date(time),
+                temperature: hourlyWeather.temperature2m[index],
+                rain: hourlyWeather.rain[index],
+            });
+            return acc;
+        }, {});
     }
 
-    function toggleDaily() {
-        showDaily = !showDaily;
-        if (showDaily) showHourly = false;
+    function getDayOfWeek(date) {
+        return new Date(date).toLocaleDateString("en-US", { weekday: "long" });
+    }
+
+    function switchTab(tab) {
+        activeTab = tab;
     }
 </script>
 
-
 <main>
-<div class="center">
-  <h1 class="text-3xl font-bold underline" style="margin-top: 150px; text-align: center; margin-bottom: 100px;">
-	Challenge 21 
-  </h1>
-</div>
-
-<div class="center">
-    <div class="picture-div">
-      <img src="https://t4.ftcdn.net/jpg/04/85/17/33/360_F_485173339_rHVWOWEwZneJpHqgFNiYJLSPQg7hNoAA.jpg" alt="Santa Picture" style="border-radius: 20%; margin-top: 80px; width: 500px; height: 170px;">
+    <div class="center">
+        <h1 class="main-title">Challenge 21</h1>
     </div>
-</div>
 
-<div class="center">
-<h3 class="text-3xl font-bold underline center" style="margin-top: 200px;">
-Weather Application
-</h3>
-</div>
+    <div class="center">
+        <div class="picture-div">
+            <img
+                src="https://t4.ftcdn.net/jpg/04/85/17/33/360_F_485173339_rHVWOWEwZneJpHqgFNiYJLSPQg7hNoAA.jpg"
+                alt="Santa Picture"
+                class="santa-picture"
+            />
+        </div>
+    </div>
 
-<div class="button-container center">
-    <HourlyWeatherButton on:click={toggleHourly}>Show Hourly Weather</HourlyWeatherButton>
-    <DailyWeatherButton on:click={toggleDaily}>Show Daily Weather</DailyWeatherButton>
-</div>
+    <div class="center">
+        <h3 class="weather-title">Weather Application</h3>
+    </div>
 
-<!-- Hourly Weather -->
-{#if showHourly && hourlyWeather}
-    <div class="weather-section text-center" in:fade>
-        <h4>Hourly Weather</h4>
-        {#each hourlyWeather.time as time, index}
-            <div class="weather-item" in:fly={{ x: 200, delay: 100 }}>
-                <p>{time.toLocaleString()}</p>
-                <p>Temp: {hourlyWeather.temperature2m[index].toFixed(2)}°C</p>
-                <p>Rain: {hourlyWeather.rain[index].toFixed(2)}mm</p>
+    <div class="tab-container center">
+        <button on:click={() => switchTab("hourly")}>Hourly Weather</button>
+        <button on:click={() => switchTab("daily")}>Daily Weather</button>
+    </div>
+
+    <!-- Hourly Weather -->
+    {#if activeTab === "hourly" && hourlyWeather}
+        {#each Object.keys(hourlyWeatherByDay) as day}
+            <div class="day-section">
+                <h3 class="day-title">{getDayOfWeek(day)}</h3>
+                <div class="weather-grid" in:fade>
+                    {#each hourlyWeatherByDay[day] as weather}
+                        <div class="weather-card">
+                            {#if weather.rain > 0}
+                            <img
+                                src={"https://cdn-icons-png.flaticon.com/128/3313/3313888.png"}
+                                alt="Rain"
+                                class="rain-icon"
+                            />
+                        
+                        {:else if weather.rain === 0}
+                            <img
+                                src={"https://cdn-icons-png.flaticon.com/128/3313/3313896.png"}
+                                alt="Sunny"
+                                class="sunny-icon"
+                            />
+                            {/if}
+                            <p class="weather-time">
+                                {weather.time.toLocaleTimeString()}
+                            </p>
+                            <p class="weather-temp">
+                                Temp: {weather.temperature.toFixed(2)}°C
+                            </p>
+                            <p class="weather-rain">
+                                Rain: {weather.rain.toFixed(2)}mm
+                            </p>
+                         
+
+                        </div>
+                    {/each}
+                </div>
             </div>
         {/each}
-    </div>
-{/if}
+    {/if}
 
-<!-- Daily Weather -->
-{#if showDaily && dailyWeather}
-    <div class="weather-section text-center" in:fade>
-        <h4>Daily Weather</h4>
-        {#each dailyWeather.time as time, index}
-            <div class="weather-item" in:fly={{ x: 200, delay: 100 }}>
-                <p>{time.toDateString()}</p>
-                <p>Max Temp: {dailyWeather.temperature2mMax[index].toFixed(2)}°C</p>
-                <p>Min Temp: {dailyWeather.temperature2mMin[index].toFixed(2)}°C</p>
-                <p>UV Index: {dailyWeather.uvIndexMax[index].toFixed(2)}</p>
-                <p>Precipitation: {dailyWeather.precipitationSum[index].toFixed(2)}mm</p>
-            </div>
-        {/each}
-    </div>
-{/if}
+    <!-- Daily Weather -->
+    {#if activeTab === "daily" && dailyWeather}
+        <div class="weather-grid" in:fade>
+            {#each dailyWeather.time as time, index}
+                <div class="weather-card">
+                    <p class="weather-time">{time.toDateString()}</p>
+                    <p class="weather-temp">
+                        Max Temp: {dailyWeather.temperature2mMax[index].toFixed(
+                            2,
+                        )}°C
+                    </p>
+                    <p class="weather-temp">
+                        Min Temp: {dailyWeather.temperature2mMin[index].toFixed(
+                            2,
+                        )}°C
+                    </p>
+                    <p class="weather-uv">
+                        UV Index: {dailyWeather.uvIndexMax[index].toFixed(2)}
+                    </p>
+                    <p class="weather-precip">
+                        Precipitation: {dailyWeather.precipitationSum[
+                            index
+                        ].toFixed(2)}mm
+                    </p>
+                </div>
+            {/each}
+        </div>
+    {/if}
 
-<style>
-    .center {
-        display: grid;
-        place-items: center;
-    }
-    .button-container {
-        display: flex;
-        justify-content: center;
-        gap: 20px;
-    }
-    .weather-section {
-        margin: 20px;
-        padding: 10px;
-        border: 1px solid #ccc;
-    }
-    .weather-item {
-        margin-bottom: 10px;
-    }
-</style>
+    <style>
+        .main-title {
+            font-size: 3em;
+            font-weight: bold;
+            text-decoration: underline;
+            text-align: center;
+            margin-top: 150px;
+            margin-bottom: 100px;
+            color: #2a9d8f;
+        }
+        .santa-picture {
+            border-radius: 20%;
+            margin-top: 80px;
+            width: 500px;
+            height: 170px;
+            box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+        }
+        .weather-title {
+            font-size: 2.5em;
+            font-weight: bold;
+            text-align: center;
+            margin-top: 200px;
+            color: #264653;
+        }
+        .center {
+            display: grid;
+            place-items: center;
+        }
+        .tab-container {
+            display: flex;
+            justify-content: center;
+            gap: 20px;
+            margin-top: 20px;
+        }
+        .weather-grid {
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+            gap: 20px;
+            padding: 20px;
+        }
+        .weather-card {
+            border: 1px solid #ccc;
+            padding: 20px;
+            border-radius: 10px;
+            text-align: center;
+            background-color: #f3f3f3;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+            transition:
+                transform 0.3s,
+                box-shadow 0.3s;
+        }
+        .weather-time {
+            font-size: 1.2em;
+            font-weight: bold;
+            margin-bottom: 10px;
+        }
+        .weather-temp,
+        .weather-rain,
+        .weather-uv,
+        .weather-precip {
+            font-size: 1em;
+            margin: 5px 0;
+        }
+        .weather-card:hover {
+            transform: scale(1.05);
+            box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3);
+        }
+        .day-section {
+            margin: 20px 0;
+        }
+        .day-title {
+            font-size: 1.4em;
+            margin-left: 20px;
+        }
+        .rain-icon {
+            width: 20px;
+            height: 20px;
+            position: absolute;
+            top: 10px;
+            right: 10px;
+        }
+        .sunny-icon {
+            width: 20px;
+            height: 20px;
+            position: absolute;
+            top: 10px;
+            right: 10px;
+        }
+    </style>
 </main>
